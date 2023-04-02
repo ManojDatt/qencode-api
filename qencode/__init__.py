8ede9bfcf0b8e9331dd5741da2c4a255e66441df
Manoj datt <Manojdatt1it@gmail.com>
2023-04-02 21:46:06 +0530

    Update __init__.py

--- qencode/__init__.py
+++ qencode/__init__.py
@@ -1,41 +1,41 @@
 def client(api_key, api_url=None, version=None, **kwargs):
-    from client import QencodeApiClient
+    from qencode.client import QencodeApiClient
 
     return QencodeApiClient(api_key, api_url=api_url, version=version, **kwargs)
 
 
 def custom_params():
-    from custom_params import CustomTranscodingParams
+    from qencode.custom_params import CustomTranscodingParams
 
     return CustomTranscodingParams()
 
 
 def format():
-    from custom_params import Format
+    from qencode.custom_params import Format
 
     return Format()
 
 
 def destination():
-    from custom_params import Destination
+    from qencode.custom_params import Destination
 
     return Destination()
 
 
 def stream():
-    from custom_params import Stream
+    from qencode.custom_params import Stream
 
     return Stream()
 
 
 def x264_video_codec():
-    from custom_params import Libx264_VideoCodecParameters
+    from qencode.custom_params import Libx264_VideoCodecParameters
 
     return Libx264_VideoCodecParameters()
 
 
 def x265_video_codec():
-    from custom_params import Libx265_VideoCodecParameters
+    from qencode.custom_params import Libx265_VideoCodecParameters
 
     return Libx265_VideoCodecParameters()
 
